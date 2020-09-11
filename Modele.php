2dcd37cdaa8f98f0a0a943a4504e51edb49c838a
said777max <59210497+said777max@users.noreply.github.com>
2020-09-11 18:13:17 +0200

    Update Modele.php

--- Modele.php
+++ Modele.php
@@ -1,6 +1,6 @@
 <?php
 
-// Renvoie la liste des voitures du blog
+// Renvoie la liste des voitures
 function getVoitures() {
     $bdd = getBdd();
     $voitures = $bdd->query('select * from vehicules'
@@ -23,7 +23,7 @@ function getVoiture($id) {
 // Effectue la connexion à la BDD
 // Instancie et renvoie l'objet PDO associé
 function getBdd() {
-    $bdd = new PDO('mysql:host=localhost;dbname=location_vehicule;charset=utf8', 'root',
+    $bdd = new PDO('mysql:host=localhost;dbname=location_vehicules;charset=utf8', 'root',
             '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     return $bdd;
 }
